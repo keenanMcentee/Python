5e7f61794f212f35ee30474c50e7a83ac8fe6b86
keenanmcentee <c00201892@itcarlow.ie>
2017-10-26 17:16:45 +0100

    Leaderboard and Loop working

--- Word Game/wordGame.py
+++ Word Game/wordGame.py
@@ -1,7 +1,8 @@
 import random 
 import time
+import pickle
+from itertools import islice
 from collections import Counter
-
 with open('bigWords.txt', errors='ignore') as df:
     rawdataSources = df.read()
 source_words_list = rawdataSources.split()
@@ -10,18 +11,47 @@ with open('words.txt', errors='ignore') as df:
     rawdataDictionary = df.read()  
 allWords = rawdataDictionary.split()
 
+def save_score(score, name):
+    leaderboard = pickle.load( open( "leaderboard.p", "rb" ) )
+    print(leaderboard)
+    leaderboard[score] = name
+    print(leaderboard)
+    pickle.dump(leaderboard, open( "leaderboard.p", "wb" ) )
+    
+def load_scores():
+    leaderboard = pickle.load( open( "leaderboard.p", "rb" ) )
+    sorted_scores = {(k, leaderboard[k]) for k in sorted(leaderboard, key=leaderboard.get, reverse=True)}
+    top_ten = list(islice(sorted_scores, 10))
+    pos = 1
+    print("\n \n \n \nTop 10 scores: ")
+    for row in top_ten:
+        print("     ", pos, '. ', row[1], row[0])
+        pos += 1
+
+    
+def check_in_given_word(given_word, to_check):
+        '''Subtracts letters from one word to another so we can find if any invalid letters used.'''
+        given_word_counter = Counter(given_word)
+        word_counter = Counter(to_check)
+        given_word_counter.subtract(word_counter)
+        return (given_word_counter)
+
 replay = True
 while replay == True:
+    start = 0.0
+    end = 0.0
+    time_taken = 0.0
     print("Rules:")
-    print('''1. Each word must be made up from letters contained within the source word \n
-    2. The words all have 3 or more letters. \n
-    3. There are no duplicate words \n
+    print('''
+    1. Each word must be made up from letters contained within the source word 
+    2. The words all have 3 or more letters. 
+    3. There are no duplicate words 
     4. None of the 7 submitted words is the source word''')
 
     inpur_var = 0
-    input_var = input("Are you ready to start? \n ")
+    input_var = input("Enter Y to start: ")
     while (input_var[0] != 'y' and input_var[0] != 'Y'):
-        input_var = input("Error, please reply Y / N! \n")
+        input_var = input("Error, please enter Y to start: \n")
 
     source_word = random.choice(source_words_list)
     print("Please enter 7 words seperated by a space")
@@ -31,14 +61,7 @@ while replay == True:
     player_input = input("")
     player_words = player_input.split(' ')
 
-    def check_in_given_word(given_word, to_check):
-        '''Subtracts letters from one word to another so we can find if any invalid letters used.'''
-        given_word_counter = Counter(given_word)
-        word_counter = Counter(to_check)
-        given_word_counter.subtract(word_counter)
-        #if any -ve letter count is found, it is not in given_word
-        # print the counter for your info
-        return (given_word_counter)
+    print("\n\n\n\n\n")
 
     rules_followed = True
 
@@ -69,13 +92,16 @@ while replay == True:
         if word not in allWords:
             print("Made up word: " + word)
             rules_followed = False
-
+    rules_followed = True
     if rules_followed == True:
         end = time.time()
-        time = end - start
-        print("Time taken: " + "{0:.2f}".format(time) + "s")
+        time_taken = end - start
+        print("Time taken: " + "{0:.2f}".format(time_taken) + "s")
+        name = input("Enter name: ")
+        save_score(time_taken, name)
         
+    load_scores()
     want_replay = input("Would you like to play again?")
-    if input_var[0] != 'y' and input_var[0] != 'Y':
+    if want_replay[0] != 'y' and want_replay[0] != 'Y':
         replay = False;
 
